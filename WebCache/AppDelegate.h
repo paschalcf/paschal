eaf70bbe5d8a732a20304ec847468b2b1b46d68d
LOVE_PC <pangc1989@163.com>
2015-02-02 16:49:13 +0800

    Initial Commit

---
+++ WebCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebCache
+//
+//  Created by paschal on 15-2-2.
+//  Copyright (c) 2015年 caijing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebCache
+//
+//  Created by paschal on 15-2-2.
+//  Copyright (c) 2015年 caijing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
